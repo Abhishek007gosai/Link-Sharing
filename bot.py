7af211c7810fe61a7df3040180e408c0a172521f
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-06 01:32:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,3 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         self.LOGGER(__name__).info("Bot stopped.")
-
-    def run(self):
-        asyncio.run(self.start())
-    
